682d1e6af725591c9caa53a9c02dfb9e44683bd8
JOSE CAETANO BEUKER <jcbeuker@yahoo.com.br>
2018-12-22 19:52:58 -0200

    Arquivos do curso parte 1.

--- curso-java-parte-1/Programa.java
+++ curso-java-parte-1/Programa.java
@@ -1,6 +1,8 @@
 public class Programa {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
 }
